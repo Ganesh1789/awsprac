66fc7fe3e773bdae5e08f5a94ea974043804631a
Ganesh1789 <patilganesh1406@gmail.com>
2025-11-07 01:02:00 +0530

    Add Hello class with main method for CI/CD demo

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("✅ Hello Jenkins CI/CD using AWS EC2 and GitHub!");
+        System.out.println("🚀 Automated Build & Deployment Successful!");
+    }
+}
